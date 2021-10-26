06a8f9e011af1014eb85fe60806f7cd3efb3000f
Roberto <robertomartinonwg@gmail.com>
2021-10-26 15:01:27 +0200

    Stampa Hello world

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//web.php è il nostro vigile. 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
